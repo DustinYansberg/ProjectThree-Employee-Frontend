18432ef47ac7614cfe32ea0d4c382369b2870edd
Yuri Beneche <yurisenai742@gmail.com>
2024-08-15 15:29:09 -0400

    switching to sharktales

---
+++ project-2-Angular/src/app/layout/app.menu.component.ts
@@ -0,0 +1,32 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LayoutService } from './service/app.layout.service';
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './app.menu.component.html'
+})
+export class AppMenuComponent implements OnInit {
+
+    model: any[] = [];
+
+    constructor(public layoutService: LayoutService) { }
+
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Home',
+                items: [
+                    //Change to /employeedetail/ (pass id from the login)
+                    { label: 'My Info', icon: 'pi pi-fw pi-home', routerLink: ['/employeedetail/{id}'] },
+                    //Change to view accounts rather than creating your own you must request them
+                    { label: 'My Accounts', icon: 'pi pi-fw pi-sitemap', routerLink: ['/accounts'] },
+                    // Roles with all role info to fill out the page, can request R and E from here
+                    { label: 'My Roles and Entitlements', icon: 'pi pi-fw pi-users', routerLink: ['/permissions'] },
+                    // some frontend plugin or bonus feature
+                    { label: 'IIQ Plugin', icon: 'pi pi-fw pi-check', routerLink: ['/plugin'] }
+                ]
+            }
+        ];
+    }
+}
