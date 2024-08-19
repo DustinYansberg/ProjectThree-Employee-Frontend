212286129f4cc317954d871c107a5b1b55cb4313
gabeguio <gabe.guio@gmail.com>
2024-08-19 14:44:46 -0400

    Disable authGaurd and 404 reroute

--- project-2-Angular/src/app/app-routing.module.ts
+++ project-2-Angular/src/app/app-routing.module.ts
@@ -10,11 +10,11 @@ import { AuthGuard } from './Services/auth.guard';
             {
                 path: '', component: AppLayoutComponent,
                 children: [
-                    { path: '', canActivate: [AuthGuard], loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-                    { path: 'employees', canActivate: [AuthGuard], loadChildren: () => import('./components/employees/employees.module').then(m => m.EmployeesModule) },
-                    { path: 'employeedetail/:id', canActivate: [AuthGuard], loadChildren: () => import('./components/employee-detail/employee-detail.module').then(m => m.EmployeeDetailModule) },
-                    { path: 'accounts', canActivate: [AuthGuard], loadChildren: () => import('./components/accounts/accounts.module').then(m => m.AccountsModule) },
-                    { path: 'accountdetail/:id', canActivate: [AuthGuard], loadChildren: () => import('./components/account-detail/account-detail.module').then(m => m.AccountDetailModule) },
+                    { path: '', /*canActivate: [AuthGuard],*/ loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+                    { path: 'employees', /*canActivate: [AuthGuard],*/ loadChildren: () => import('./components/employees/employees.module').then(m => m.EmployeesModule) },
+                    { path: 'employeedetail/:id', /*canActivate: [AuthGuard],*/ loadChildren: () => import('./components/employee-detail/employee-detail.module').then(m => m.EmployeeDetailModule) },
+                    { path: 'accounts', /*canActivate: [AuthGuard],*/ loadChildren: () => import('./components/accounts/accounts.module').then(m => m.AccountsModule) },
+                    { path: 'accountdetail/:id', /*canActivate: [AuthGuard],*/ loadChildren: () => import('./components/account-detail/account-detail.module').then(m => m.AccountDetailModule) },
                     { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
                     { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
                     { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
@@ -24,7 +24,7 @@ import { AuthGuard } from './Services/auth.guard';
             },
             { path: 'auth', loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule) },
             { path: 'landing', loadChildren: () => import('./components/landing/landing.module').then(m => m.LandingModule) },
-            { path: 'notfound', component: NotfoundComponent },
+            // { path: 'notfound', component: NotfoundComponent },
             { path: '**', redirectTo: '/notfound' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
     ],
