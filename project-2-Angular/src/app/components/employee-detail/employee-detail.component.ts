6dd2ddc03fc4a05fb0579282ae3a79cad1011376
DustinYansberg <DustinYansberg@gmail.com>
2024-08-20 13:48:32 -0700

    populating employee profile with dummy data

--- project-2-Angular/src/app/components/employee-detail/employee-detail.component.ts
+++ project-2-Angular/src/app/components/employee-detail/employee-detail.component.ts
@@ -7,43 +7,70 @@ import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
 @Component({
-    templateUrl: './employee-detail.component.html',
-    providers: [MessageService]
+  templateUrl: './employee-detail.component.html',
+  providers: [MessageService],
 })
 export class EmployeeDetailComponent implements OnInit {
+  employeeDetails: EmployeeDetail;
 
-    employeeDetails: EmployeeDetail;
+  primaryEmail: string;
 
-    primaryEmail: string;
+  constructor(
+    private employeeService: EmployeeService,
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-    constructor(private employeeService: EmployeeService, private messageService: MessageService, private route: ActivatedRoute, private router: Router) {}
+  ngOnInit() {
+    // TODO - Implement the getEmployeeById method in the employee service
+    // TODO - How are we actually going to get the employee ID?
+    // this.employeeService
+    //   .getEmployeeById(this.route.snapshot.params['id'])
+    //   .subscribe({
+    //     next: (response) => {
+    //       let employeeResponse: any = response.body;
+    //       console.log(employeeResponse);
+    //       this.employeeDetails = new EmployeeDetail(
+    //         employeeResponse.id || '',
+    //         employeeResponse.userName || '',
+    //         employeeResponse.displayName || '',
+    //         employeeResponse.active || false,
+    //         employeeResponse.emails[0].value
+    //           ? employeeResponse.emails[0].value
+    //           : 'N/A',
+    //         new Meta(
+    //           new Date(employeeResponse.meta.created),
+    //           new Date(employeeResponse.meta.lastModified)
+    //         ),
+    //         employeeResponse.isManager || false,
+    //         employeeResponse.riskScore || 0,
+    //         employeeResponse[
+    //           'urn:ietf:params:scim:schemas:extension:enterprise:2.0:User'
+    //         ].manager.displayName || 'N/A'
+    //       );
+    //     },
 
-    ngOnInit() {
-        this.employeeService.getEmployeeById(this.route.snapshot.params['id'])
-        .subscribe({
-            next: (response) => {
-                let employeeResponse: any = response.body;
-                console.log(employeeResponse)
-                this.employeeDetails = new EmployeeDetail(
-                    employeeResponse.id || '',
-                    employeeResponse.userName || '',
-                    employeeResponse.displayName || '',
-                    employeeResponse.active || false,
-                    employeeResponse.emails[0].value ? employeeResponse.emails[0].value : 'N/A',
-                    new Meta(
-                        new Date(employeeResponse.meta.created),
-                        new Date(employeeResponse.meta.lastModified),
-                    ),
-                    employeeResponse.isManager || false,
-                    employeeResponse.riskScore || 0,
-                    employeeResponse["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].manager.displayName || 'N/A'
-                );
-
-            },
-            error: (err) => {
-                this.messageService.add({ severity: 'error', summary: 'Error', detail: err.message, life: 3000 });
-                this.router.navigate(['/notfound']);
-            }
-        });
-    }
+    //     error: (err) => {
+    //       this.messageService.add({
+    //         severity: 'error',
+    //         summary: 'Error',
+    //         detail: err.message,
+    //         life: 3000,
+    //       });
+    //       this.router.navigate(['/notfound']);
+    //     },
+    //   });
+    this.employeeDetails = new EmployeeDetail(
+      '111111',
+      'jDoeFoSho',
+      'John Doe',
+      true,
+      'N/A',
+      new Meta(new Date(), new Date()),
+      false,
+      0,
+      'N/A'
+    );
+  }
 }
