6cf5a17259499f3fe295b640a555a6d241cd03fa
DustinYansberg <DustinYansberg@gmail.com>
2024-08-21 08:23:38 -0700

    login routes to home page. logout logs user out of auth

--- project-2-Angular/src/app/components/auth/buttons/login-button.component.ts
+++ project-2-Angular/src/app/components/auth/buttons/login-button.component.ts
@@ -13,7 +13,7 @@ export class LoginButtonComponent {
   handleLogin(): void {
     this.auth.loginWithRedirect({
       appState: {
-        target: '/auth/profile',
+        target: '/',
       },
     });
   }
--- project-2-Angular/src/app/layout/app.topbar.component.ts
+++ project-2-Angular/src/app/layout/app.topbar.component.ts
@@ -1,104 +1,117 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { MenuItem } from 'primeng/api';
-import { LayoutService } from "./service/app.layout.service";
-import { AuthService } from '../Services/auth.service';
+import { LayoutService } from './service/app.layout.service';
+import { AuthService } from '@auth0/auth0-angular';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-topbar',
-    templateUrl: './app.topbar.component.html'
+  selector: 'app-topbar',
+  templateUrl: './app.topbar.component.html',
 })
 export class AppTopBarComponent {
+  private auth = inject(AuthService);
+  private doc = inject(DOCUMENT);
 
-    @ViewChild('menubutton') menuButton!: ElementRef;
+  @ViewChild('menubutton') menuButton!: ElementRef;
 
-    @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
+  @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
 
-    @ViewChild('topbarmenu') menu!: ElementRef;
+  @ViewChild('topbarmenu') menu!: ElementRef;
 
-    @ViewChild('profileMenu') profileMenu!: any;
+  @ViewChild('profileMenu') profileMenu!: any;
 
-    constructor(public layoutService: LayoutService, public authService: AuthService, public router: Router) { }
+  constructor(
+    public layoutService: LayoutService,
+    public authService: AuthService,
+    public router: Router
+  ) {}
 
-    profileItems: MenuItem[];
+  profileItems: MenuItem[];
 
-    settingsItems: MenuItem[];
+  settingsItems: MenuItem[];
 
-    isDarkMode: boolean = false;
+  isDarkMode: boolean = false;
 
-    // Dropdown for profile items
-    ngOnInit() {
-        this.profileItems = [
-          {
-            label: 'Sign Out',
-            icon: 'pi pi-sign-out',
-            command: () => this.signOut()
-          }
-        ];
+  // Dropdown for profile items
+  ngOnInit() {
+    this.profileItems = [
+      {
+        label: 'Sign Out',
+        icon: 'pi pi-sign-out',
+        command: () => this.signOut(),
+      },
+    ];
 
-        this.settingsItems = [
-          {
-            label: 'Dark Mode',
-            icon: 'pi pi-moon',
-            command: () => this.toggleDarkMode()
-          }
-        ];
+    this.settingsItems = [
+      {
+        label: 'Dark Mode',
+        icon: 'pi pi-moon',
+        command: () => this.toggleDarkMode(),
+      },
+    ];
 
-        this.updateSettingsItems();
-      }
+    this.updateSettingsItems();
+  }
 
-      // Light/dark mode toggle
-      toggleDarkMode() {
-        this.isDarkMode = !this.isDarkMode;
-        if (this.isDarkMode) {
-          this.changeTheme('bootstrap4-dark-purple', 'dark')
-        } else {
-          this.changeTheme('bootstrap4-light-blue', 'light')
-        }
+  // Light/dark mode toggle
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      this.changeTheme('bootstrap4-dark-purple', 'dark');
+    } else {
+      this.changeTheme('bootstrap4-light-blue', 'light');
+    }
 
-        this.updateSettingsItems();
-      }
+    this.updateSettingsItems();
+  }
 
-      // Update the settings icon
-      updateSettingsItems() {
-        this.settingsItems = [
-          {
-            label: this.isDarkMode ? 'Light Mode' : 'Dark Mode',
-            icon: this.isDarkMode ? 'pi pi-sun' : 'pi pi-moon',
-            command: () => this.toggleDarkMode()
-          }
-        ];
-      }
+  // Update the settings icon
+  updateSettingsItems() {
+    this.settingsItems = [
+      {
+        label: this.isDarkMode ? 'Light Mode' : 'Dark Mode',
+        icon: this.isDarkMode ? 'pi pi-sun' : 'pi pi-moon',
+        command: () => this.toggleDarkMode(),
+      },
+    ];
+  }
 
-      changeTheme(theme: string, colorScheme: string) {
-        this.theme = theme;
-        this.colorScheme = colorScheme;
-      }
-    
-      // Auth Information
-      signOut() {
-        this.authService.logout(); // Adjust this method according to your AuthService
-        this.router.navigate(['/auth/login']);
-      }
+  changeTheme(theme: string, colorScheme: string) {
+    this.theme = theme;
+    this.colorScheme = colorScheme;
+  }
 
-      // Theme for service
-      set theme(val: string) {
-        this.layoutService.config.update((config) => ({
-            ...config,
-            theme: val,
-        }));
-      }
-      get theme(): string {
-          return this.layoutService.config().theme;
-      }
+  // Auth Information
+  signOut(): void {
+    console.log(this.doc.location.origin);
+    this.auth.logout({
+      logoutParams: {
+        returnTo: this.doc.location.origin,
+      },
+    });
+    // this.authService.logout(); // Adjust this method according to your AuthService
+    // this.router.navigate(['/auth/login']);
+  }
 
-      set colorScheme(val: string) {
-          this.layoutService.config.update((config) => ({
-              ...config,
-              colorScheme: val,
-          }));
-      }
-      get colorScheme(): string {
-          return this.layoutService.config().colorScheme;
-      }
+  // Theme for service
+  set theme(val: string) {
+    this.layoutService.config.update((config) => ({
+      ...config,
+      theme: val,
+    }));
+  }
+  get theme(): string {
+    return this.layoutService.config().theme;
+  }
+
+  set colorScheme(val: string) {
+    this.layoutService.config.update((config) => ({
+      ...config,
+      colorScheme: val,
+    }));
+  }
+  get colorScheme(): string {
+    return this.layoutService.config().colorScheme;
+  }
 }
