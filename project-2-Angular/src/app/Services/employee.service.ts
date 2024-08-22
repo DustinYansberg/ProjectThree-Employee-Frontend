71786c1a080727f1400e74a81d13c687a9504588
DustinYansberg <DustinYansberg@gmail.com>
2024-08-22 08:12:32 -0700

    making it so we can get employee by the email that was used by auth0 to sign in

--- project-2-Angular/src/app/Services/employee.service.ts
+++ project-2-Angular/src/app/Services/employee.service.ts
@@ -5,33 +5,53 @@ import { AuthService } from './auth.service';
 
 @Injectable()
 export class EmployeeService {
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
-
-  url: String = 'http://localhost:3000/';
+  url: string = 'http://4.156.40.62:9001/employee';
+  // url: string = 'http://localhost:9001/employee';
 
   private getHeaders() {
     return this.authService.getHeaders();
   }
 
   getAllEmployees() {
-    return this.http.get(this.url + 'api/users', { headers: this.getHeaders(), observe: 'response' });
+    return this.http.get(this.url, {
+      headers: this.getHeaders(),
+      observe: 'response',
+    });
   }
 
   getEmployeeById(id: string) {
-    return this.http.get(this.url + 'api/users/' + id, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.get(this.url + '/' + id, {
+      headers: this.getHeaders(),
+      observe: 'response',
+    });
+  }
+  getEmployeeByEmail(email: string) {
+    return this.http.get(this.url + '/email/' + email, {
+      headers: this.getHeaders(),
+      observe: 'response',
+    });
   }
 
   createEmployee(employee: Employee) {
-    return this.http.post(this.url + 'api/users', employee, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.post(this.url + 'api/users', employee, {
+      headers: this.getHeaders(),
+      observe: 'response',
+    });
   }
 
   updateEmployee(employee: Employee) {
-    return this.http.put(this.url + 'api/users/' + employee.id, employee, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.put(this.url + 'api/users/' + employee.id, employee, {
+      headers: this.getHeaders(),
+      observe: 'response',
+    });
   }
 
   deleteEmployee(id: string) {
-    return this.http.delete(this.url + 'api/users/' + id, { headers: this.getHeaders(), observe: 'response' });
+    return this.http.delete(this.url + 'api/users/' + id, {
+      headers: this.getHeaders(),
+      observe: 'response',
+    });
   }
-
 }
--- project-2-Angular/src/app/components/employee-detail/employee-detail.component.ts
+++ project-2-Angular/src/app/components/employee-detail/employee-detail.component.ts
@@ -3,47 +3,67 @@ import { Meta } from 'src/app/Models/meta';
 import { EmployeeDetail } from 'src/app/Models/employeeDetails';
 import { EmployeeService } from 'src/app/Services/employee.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
-    templateUrl: './employee-detail.component.html',
-    providers: [MessageService]
+  templateUrl: './employee-detail.component.html',
+  providers: [MessageService],
 })
 export class EmployeeDetailComponent implements OnInit {
+  private auth = inject(AuthService);
+  user$ = this.auth.user$;
+  private user: any;
 
-    employeeDetails: EmployeeDetail;
+  employeeDetails: EmployeeDetail;
 
-    primaryEmail: string;
+  primaryEmail: string;
 
-    constructor(private employeeService: EmployeeService, private messageService: MessageService, private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private employeeService: EmployeeService,
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-    ngOnInit() {
-        this.employeeService.getEmployeeById(this.route.snapshot.params['id'])
-        .subscribe({
-            next: (response) => {
-                let employeeResponse: any = response.body;
-                console.log(employeeResponse)
-                this.employeeDetails = new EmployeeDetail(
-                    employeeResponse.id || '',
-                    employeeResponse.userName || '',
-                    employeeResponse.displayName || '',
-                    employeeResponse.active || false,
-                    employeeResponse.emails[0].value ? employeeResponse.emails[0].value : 'N/A',
-                    new Meta(
-                        new Date(employeeResponse.meta.created),
-                        new Date(employeeResponse.meta.lastModified),
-                    ),
-                    employeeResponse.isManager || false,
-                    employeeResponse.riskScore || 0,
-                    employeeResponse["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].manager.displayName || 'N/A'
-                );
-
-            },
-            error: (err) => {
-                this.messageService.add({ severity: 'error', summary: 'Error', detail: err.message, life: 3000 });
-                this.router.navigate(['/notfound']);
-            }
+  ngOnInit() {
+    this.user$.subscribe((user) => {
+      if (user) {
+        this.employeeService.getEmployeeByEmail(user.email).subscribe({
+          next: (response) => {
+            let employeeResponse: any = response.body;
+            console.log(employeeResponse);
+            this.employeeDetails = new EmployeeDetail(
+              employeeResponse.id || '',
+              employeeResponse.userName || '',
+              employeeResponse.displayName || '',
+              employeeResponse.active || false,
+              employeeResponse.emails[0].value
+                ? employeeResponse.emails[0].value
+                : 'N/A',
+              new Meta(
+                //   new Date(employeeResponse.meta.created),
+                //   new Date(employeeResponse.meta.lastModified)
+                new Date(),
+                new Date()
+              ),
+              employeeResponse.isManager || false,
+              employeeResponse.riskScore || 0,
+              employeeResponse.manager?.displayName || 'N/A'
+            );
+          },
+          error: (err) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: err.message,
+              life: 3000,
+            });
+            this.router.navigate(['/notfound']);
+          },
         });
-    }
+      }
+    });
+  }
 }
