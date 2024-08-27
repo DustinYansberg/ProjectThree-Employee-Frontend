79e5132d700b4ffb9fd4d79557d52833b3b6b087
Yuri Beneche <yurisenai742@gmail.com>
2024-08-27 10:28:17 -0400

    needs to merge auth

--- project-2-Angular/src/app/Services/request.service.ts
+++ project-2-Angular/src/app/Services/request.service.ts
@@ -1,36 +1,45 @@
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Employee } from '../Models/employee';
+// import { AuthService } from './auth.service';
 import { Request } from '../Models/request';
 
 @Injectable()
 export class RequestService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    //  private authService: AuthService
+    ) { }
 
-  url: String = 'http://4.156.40.62:9001/';
+  // url: String = 'http://4.156.40.62:9001/entitlements/';
+  url: String = 'http://localhost:8081/entitlements/';
 
-  getAllRequests() {
-    return this.http.get(this.url + 'request', { observe: 'response' });
+  private getHeaders() {
+    return null //this.authService.getHeaders();
   }
 
-  getRequestById(id: string) {
-    return this.http.get(this.url + 'request/' + id, { observe: 'response' });
+  getByApp(app: string) {
+    return this.http.get(this.url + 'getByApp/' + app, { observe: 'response' });
   }
 
-  createRequest(request: Request) {
-    return this.http.post(this.url + 'request', request, { observe: 'response' });
+  getByManager(managerId : string) {
+    return this.http.get(this.url + 'getByOwner/' + managerId, { observe: 'response' });
   }
 
-  updateRequest(request: Request) {
-    return this.http.put(this.url + 'request/' + request.id, request, { observe: 'response' });
+  getByManagerAndStatus(managerId: string, processed : boolean) {
+    return this.http.get(this.url + 'getByManagerAndStatus/' + 
+      managerId +"/"+ processed, { headers: this.getHeaders(), observe: 'response' });
   }
 
-  denyRequest(id: string) {
-    return this.http.delete(this.url + 'request/' + id, { observe: 'response' });
+  createRequest(manId:string, entId:string, userId:string, description:string) {
+    return this.http.post(this.url + 'request/'+ manId +"/"+ entId +"/"+
+       userId +"/"+ description, { headers: this.getHeaders(), observe: 'response' });
   }
 
-  approveRequest(id: string) {
-    return this.http.put(this.url + 'request/' + id, { observe: 'response' });
+  processRequest(userId: string, entitlementId: string, choice: boolean) {
+    return this.http.put(this.url + 'process/' + userId +"/"+ entitlementId +"/"+ choice, { headers: this.getHeaders(), observe: 'response' });
   }
+
+
 
 }
